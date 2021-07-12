7cade36234b5f775abf508254570a44485525c61
kiruthihan nagarajan <47429159+kiruthihan10@users.noreply.github.com>
2021-07-12 21:49:33 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
+   //Change server details as you wish
    $servername = "YOUR_SERVER_NAME_HERE";
-   $username = "USERNAME_HERE";
-   $password = "PASSWORD_HERE";
+   $username = "root";
+   $password = "";
    $dbname = "DATABASE_NAME_HERE";
    $conn = new mysqli($servername, $username, $password, $dbname);
    if ($conn->connect_error) {
