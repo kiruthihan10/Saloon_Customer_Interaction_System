6a84070557a6d3c5552df4f40e3e4a6e8d643162
kiruthihan nagarajan <47429159+kiruthihan10@users.noreply.github.com>
2021-12-12 11:49:47 +0530

    Make the Database Online
    Online SQL allows the website to be deployed

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-   $servername = "localhost";
-   $username = "root";
-   $password = "";
-   $dbname = "hscmsdb";
+   $servername = "sql6.freemysqlhosting.net";
+   $username = "sql6458218";
+   $password = "cuMmBigGTg";
+   $dbname = "sql6458218";
    $conn = new mysqli($servername, $username, $password, $dbname);
    if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
