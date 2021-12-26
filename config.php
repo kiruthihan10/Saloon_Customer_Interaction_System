fb1b3cf1dcff4e902ac67936c1f031cc98bea4f2
kiruthihan nagarajan <47429159+kiruthihan10@users.noreply.github.com>
2021-12-26 12:46:48 +0530

    Update config.php
    SQL config Update

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-   $servername = "sql6.freemysqlhosting.net";
-   $username = "sql6458218";
-   $password = "cuMmBigGTg";
-   $dbname = "sql6458218";
+   $servername = "sql209.epizy.com";
+   $username = "epiz_30671718";
+   $password = "NgjQL0peh5zDWp";
+   $dbname = "epiz_30671718_saloon_database";
    $conn = new mysqli($servername, $username, $password, $dbname);
    if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
